4df4afaea65a5bc8925b33d604abc0edadf316e6
thephilross <philippross369@gmail.com>
2016-07-11 11:31:42 -0400

    update

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 set -eu
 
 for file in $(ls *.md); do
-	pandoc -f markdown_github -t html5 -c style.css --toc $file > ${file%.*}.html
+	pandoc -f markdown_github+yaml_metadata_block -t html5 -c style.css --toc $file > ${file%.*}.html
 done
 
 git add --all
